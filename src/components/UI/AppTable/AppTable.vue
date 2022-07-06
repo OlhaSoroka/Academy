87282b7aefab6b12053f50c74131ee36a29fc87a
Vladyslav <baskov228322@gmail.com>
2022-07-06 22:24:58 +0300

    sort and header done

--- src/components/UI/AppTable/AppTable.vue
+++ src/components/UI/AppTable/AppTable.vue
@@ -5,44 +5,37 @@
     rounded-2xl	
     shadow-lg
     w-11/12
-    table-auto
     m-auto"
         v-if="!isDataLoading"
     >
         <thead class="table-head">
-            <slot name="table-head"></slot>
+            <AppTableRow>
+                <th
+                    v-for="(item) in getEntriesFromArray(headerData)"
+                    :key="item[0]"
+                    @click="sortTable(item[0])"
+                >{{ item[1] }}</th>
+            </AppTableRow>
         </thead>
-
         <tbody>
             <AppTableRow
-                v-for="item in tableData"
+                v-for="(item) in items"
                 :key="item.id"
-                @editHandler="onEdit"
-                @deleteHandler="onDelete"
-                :propsData="item.id"
-                :deleteable="true"
-                :editable="true"
+                :editable="editBtns"
+                :deleteable="deleteBtns"
             >
-                <td class="flex flex-row items-center justify-between">
-                    <img
-                        :src="item.avatarUrl"
-                        alt=""
-                        width="50"
-                    >{{ item.fullName }}
-                </td>
-                <td>{{ item.email }}</td>
-                <td>{{ item.initialScore }}</td>
-                <td>{{ item.role }}</td>
-                <td>{{ item.course }}</td>
+                <td
+                    v-for="(prop, index) in getEntriesFromArray(headerData)"
+                    :key="index"
+                >{{ item[prop[0]] }}</td>
             </AppTableRow>
         </tbody>
 
     </table>
-
     <AppPreloader v-else />
 
 </template>
-
+ 
 <script>
 import AppTableRow from './AppTableRow.vue'
 import AppPreloader from '../Preloader/AppPreloader.vue';
@@ -51,13 +44,47 @@ export default {
         AppTableRow,
         AppPreloader
     },
-    props: ['tableData', 'isDataLoading'],
+    props: ['tableData', 'isDataLoading', 'editBtns', "deleteBtns", "headerData"],
+    data() {
+        return {
+            sort: {
+                value: null,
+                countOfClick: 1
+            },
+            items: []
+        }
+    },
+    updated() {
+        this.items = this.tableData;
+    },
     methods: {
         onEdit(id) {
             console.log(id + " Start editing");
         },
         onDelete(id) {
             console.log(id + " Start deleting");
+        },
+        getEntriesFromArray(array) {
+            return array.reduce((acc, el) => [...acc, Object.entries(el)[0]], [])
+        },
+        sortTable(newValue) {
+            if (this.sort.value === newValue) {
+                this.$set(this.sort, 'countOfClick', this.sort.countOfClick + 1)
+            } else {
+                this.$set(this.sort, 'value', newValue)
+                this.$set(this.sort, 'countOfClick', 1)
+            }
+            this.items = this.items.sort((a, b) => {
+                const prop1 = a[this.sort.value];
+                const prop2 = b[this.sort.value];
+                if (this.sort.countOfClick % 2 == 0) {
+                    if (typeof prop1 === 'string') return prop1.localeCompare(prop2)
+                    if (typeof prop1 === 'number') return prop1 - prop2
+                } else {
+                    if (typeof prop1 === 'string') return prop2.localeCompare(prop1)
+                    if (typeof prop1 === 'number') return prop2 - prop1
+                }
+            })
         }
     },
 }
--- src/views/member/MemberCoursesView.vue
+++ src/views/member/MemberCoursesView.vue
@@ -1,54 +1,37 @@
 <template>
 	<AppTable
-		:tableData="arr"
+		:tableData="tableData"
 		:is-data-loading="isLoading"
-		style="margin-top: 50px;"
-	>
-		<template v-slot:table-head>
-			<AppTableRow
-				:deleteable="false"
-				:editable="false"
-			>
-				<td @click="sortTableData('fullName')">Full Name</td>
-				<td @click="sortTableData('email')">Email</td>
-				<td @click="sortTableData('initialScore')">initialScore</td>
-				<td @click="sortTableData('role')">role</td>
-				<td @click="sortTableData('course')">course</td>
-			</AppTableRow>
-		</template>
-
-	</AppTable>
+		:header-data="headingData"
+		:delete-btns="true"
+		:edit-btns="true"
+	/>
 </template>
 
 <script>
 import AppTable from '../../components/UI/AppTable/AppTable.vue'
-import AppTableRow from '../../components/UI/AppTable/AppTableRow.vue'
 export default {
 	components: {
 		AppTable,
-		AppTableRow
 	},
 	data() {
 		return {
-			arr: [],
+			tableData: [],
 			isLoading: false,
-		}
-	},
-	methods: {
-		sortTableData(prop) {
-			this.arr = this.arr.sort((a, b) => {
-				if (typeof a[prop] == 'number') {
-					return a[prop] - b[prop]
-				}
-				return a[prop].localeCompare(b[prop])
-			})
+			headingData: [
+				{ "course": "Курс" },
+				{ "role": "Роль" },
+				{ "initialScore": "хз че-это" },
+				{ "fullName": "Полное имя" },
+				{ "email": "E-mail" }
+			],
 		}
 	},
 	mounted() {
 		this.isLoading = true
 		fetch('https://inventorsoft-vue-2022-users.herokuapp.com/firebase/get-all-users')
 			.then(response => response.json())
-			.then(data => this.arr = data)
+			.then(data => this.tableData = data)
 			.finally(() => this.isLoading = false
 			)
 	}
