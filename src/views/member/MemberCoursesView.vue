f60a6dd4a036cbe254e7a5cc89738ce476093928
Vladyslav <baskov228322@gmail.com>
2022-07-07 11:01:07 +0300

    cleaned view

--- src/views/member/MemberCoursesView.vue
+++ src/views/member/MemberCoursesView.vue
@@ -1,51 +1,11 @@
 <template>
-	<AppTable
-		:tableData="tableData"
-		:is-data-loading="isLoading"
-		:header-data="headingData"
-		@onDelete="deleteRow"
-		@onEdit="editRow"
-		:delete-btns="true"
-		:edit-btns="true"
-		:logo="true"
-	/>
+	<div>MemberCoursesView</div>
 </template>
 
 <script>
-import AppTable from '../../components/UI/AppTable/AppTable.vue'
 export default {
 	components: {
-		AppTable,
 	},
-	data() {
-		return {
-			tableData: [],
-			isLoading: false,
-			headingData: [
-				{ "fullName": "Полное имя" },
-				{ "role": "Роль" },
-				{ "email": "E-mail" },
-				{ "initialScore": "Какой-то номер" },
-				{ "course": "Курс" },
-			],
-		}
-	},
-	methods: {
-		deleteRow(id) {
-			this.tableData = this.tableData.filter(el => el.id !== id)
-		},
-		editRow(id) {
-			console.log(id);
-		},
-	},
-	mounted() {
-		this.isLoading = true
-		fetch('https://inventorsoft-vue-2022-users.herokuapp.com/firebase/get-all-users')
-			.then(response => response.json())
-			.then(data => this.tableData = data)
-			.finally(() => this.isLoading = false
-			)
-	}
 };
 </script>
 
