dfa4f6d63a6768557bb383e9a2cae68751a41c6e
Vladyslav <baskov228322@gmail.com>
2022-08-04 14:19:12 +0300

    lint

--- src/components/Modals/NewApplicantModal.vue
+++ src/components/Modals/NewApplicantModal.vue
@@ -1,41 +1,41 @@
 <template>
-    <BaseModal
-        ref="newApplicantModal"
-        :header="'Add new applicant'"
-    >
-        <template #body>
-            <div class="flex justify-center flex-col mt-7  gap-10">
-                <div class="mx-1">
-                    <label
-                        for="applicants"
-                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-                    >Select an option</label>
+  <BaseModal
+    ref="newApplicantModal"
+    :header="'Add new applicant'"
+  >
+    <template #body>
+      <div class="flex justify-center flex-col mt-7  gap-10">
+        <div class="mx-1">
+          <label
+            for="applicants"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+          >Select an option</label>
 
-                    <select
-                        id='applicants'
-                        v-model="newUser"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    >
-                        <option
-                            v-for="(user) in usersWithoutApplicants"
-                            :key="user.id"
-                            :value="user.id"
-                        >
-                            {{ user.fullName }}
-                        </option>
-                    </select>
-                </div>
-                <div class="mx-1 flex gap-10">
-                    <BaseButton @click="confirmAdding({ id: currentRouteName, course: currentCourse })">
-                        Add
-                    </BaseButton>
-                    <BaseButton @click="cancelModal">
-                        Cancel
-                    </BaseButton>
-                </div>
-            </div>
-        </template>
-    </BaseModal>
+          <select
+            id="applicants"
+            v-model="newUser"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option
+              v-for="(user) in usersWithoutApplicants"
+              :key="user.id"
+              :value="user.id"
+            >
+              {{ user.fullName }}
+            </option>
+          </select>
+        </div>
+        <div class="mx-1 flex gap-10">
+          <BaseButton @click="confirmAdding({ id: currentRouteName, course: currentCourse })">
+            Add
+          </BaseButton>
+          <BaseButton @click="cancelModal">
+            Cancel
+          </BaseButton>
+        </div>
+      </div>
+    </template>
+  </BaseModal>
 </template>
 
 <script>
