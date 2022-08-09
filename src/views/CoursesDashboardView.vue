cd9d3cfe5a49f020e51038fb1e003c19d5d6be4c
AlexandrGashevsky <71451324+AlexandrGashevsky@users.noreply.github.com>
2022-08-09 23:19:05 +0300

    role check

--- src/views/CoursesDashboardView.vue
+++ src/views/CoursesDashboardView.vue
@@ -108,10 +108,18 @@ export default {
     },
     ...mapGetters("user", ["user"]),
     isUser() {
-      return this.user.role === USER_ROLE;
+       if (this.user) {
+        return this.user.role === USER_ROLE;
+      } else {
+        return false;
+      }
     },
     isManagerOrAdmin() {
-      return this.user.role === MANAGER_ROLE || ADMIN_ROLE;
+      if (this.user) {
+        return this.user.role === MANAGER_ROLE || ADMIN_ROLE;
+      } else {
+        return false;
+      }
     },
   },
   mounted() {
