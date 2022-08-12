23f417e3e216d5e1b950c3f665da1d05ac63bf85
unknown <yurchik2000@gmail.com>
2022-08-12 12:03:24 +0300

    fixed negtative number in score

--- src/components/BaseComponents/BaseInput.vue
+++ src/components/BaseComponents/BaseInput.vue
@@ -22,9 +22,12 @@
           6 chars min with at least 1 Latin letter and 1 number.
         </span>
         <span v-if="errors.length > 0 && vid === 'confirmpassword'">
-          Passwords do not match
+          Passwords do not match.
         </span>        
-        <span v-if="errors.length > 0 && vid != 'confirmpassword' && vid != 'password'">{{ errors[0] }}</span>
+        <span v-if="errors.length > 0 && label === 'Full Name'">
+          Full Name is empty.
+        </span>        
+        <span v-if="errors.length > 0 && vid != 'confirmpassword' && vid != 'password' && label != 'Full Name'">{{ errors[0] }}</span>
       </p>
     </div>
   </ValidationProvider>
@@ -75,6 +78,7 @@ export default {
     return {
       emailRules: "required|email",
       passwordRules: "required|min:6|regex:[A-Za-z]+[0-9]+",
+      initialScoreRules: "required|min_value:0"
     };
   },
   computed: {
@@ -97,7 +101,8 @@ export default {
         return `${this.emailRules}|${this.rules}`;
       } else if (this.type === "password" && this.vid != "text") {
         return `${this.passwordRules}|${this.rules}`;
-      } else return `${this.rules}`;
+      } else if (this.type === 'number')  return `${this.initialScoreRules}`;
+      else return `${this.rules}`;
     },
   },
   methods: {
