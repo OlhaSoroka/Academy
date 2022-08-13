71d191d90a250abc8257d79a0ee661ebd7d39c01
Olha Soroka <osoroka0802@gmail.com>
2022-08-13 20:15:10 +0300

    added before course details guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,125 +1,95 @@
-import Vue from "vue"
-import VueRouter from "vue-router"
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-const LoginView = () =>
-	import(/* webpackChunkName:'login'*/ "../views/LoginView.vue")
-const ResetView = () =>
-	import(/* webpackChunkName:'reset'*/ "../views/ResetView.vue")
-const NotFoundView = () =>
-	import(/* webpackChunkName:'not-found'*/ "../views/NotFoundView.vue")
-const ProfileView = () =>
-	import(/* webpackChunkName:'profile'*/ "../views/ProfileView.vue")
-const UsersView = () =>
-	import(/* webpackChunkName:'users'*/ "../views/UsersView.vue")
-const ManagersView = () =>
-	import(/* webpackChunkName:'managers'*/ "../views/ManagersView.vue")
-const CoursesView = () =>
-	import(/* webpackChunkName:'courses'*/ "../views/CoursesView.vue")
-const CourseDetailsView = () =>
-	import(
-		/* webpackChunkName:'course-details'*/ "../views/CourseDetailsView.vue"
-	)
-const CoursesDashboardView = () =>
-	import(/* webpackChunkName:'courses'*/ "../views/CoursesDashboardView.vue")
+const LoginView = () => import(/* webpackChunkName:'login'*/ '../views/LoginView.vue');
+const ResetView = () => import(/* webpackChunkName:'reset'*/ '../views/ResetView.vue');
+const NotFoundView = () => import(/* webpackChunkName:'not-found'*/ '../views/NotFoundView.vue');
+const ProfileView = () => import(/* webpackChunkName:'profile'*/ '../views/ProfileView.vue');
+const UsersView = () => import(/* webpackChunkName:'users'*/ '../views/UsersView.vue');
+const ManagersView = () => import(/* webpackChunkName:'managers'*/ '../views/ManagersView.vue');
+const CoursesView = () => import(/* webpackChunkName:'courses'*/ '../views/CoursesView.vue');
+const CourseDetailsView = () => import(/* webpackChunkName:'course-details'*/ '../views/CourseDetailsView.vue');
+const CoursesDashboardView = () => import(/* webpackChunkName:'courses'*/ '../views/CoursesDashboardView.vue');
 
-import { ADMIN_ROLE } from "@/constants/roles.constant"
-import {
-	LOGIN,
-	RESET,
-	PROFILE,
-	MANAGERS,
-	USERS,
-	COURSE_DASHBOARD,
-	COURSE_DETAILS,
-	NOT_FOUND
-} from "@/constants/routes.constant"
-import { authGuard, roleGuard } from "./utils"
-import { getAllCourses } from "../api/course/index";
+import { ADMIN_ROLE } from '@/constants/roles.constant';
+import { LOGIN, RESET, PROFILE, MANAGERS, USERS, COURSE_DASHBOARD, COURSE_DETAILS, NOT_FOUND } from '@/constants/routes.constant';
+import { authGuard, isCourseExist, roleGuard } from './utils';
 
-Vue.use(VueRouter)
-
-async function isValidId (route) {
-	getAllCourses();
-	const courses = await getAllCourses();
-	const ids = courses.map(({id}) => id);
-    if (!ids.includes(+route.params.id)) router.push({
-        name: NOT_FOUND,   params: { pathMatch: "notFound" },
-      })
-  }
+Vue.use(VueRouter);
 
 const routes = [
 	{
-		path: "/",
+		path: '/',
 		meta: { requiresAuth: true },
 		redirect: { name: COURSE_DASHBOARD }, // DEFAULT PAGE
 	},
 	{
-		path: "/login",
+		path: '/login',
 		name: LOGIN,
 		component: LoginView,
 		meta: { requiresAuth: false },
 	},
 	{
-		path: "/reset",
+		path: '/reset',
 		name: RESET,
 		component: ResetView,
 		meta: { requiresAuth: false },
 	},
 
 	{
-		path: "/profile",
+		path: '/profile',
 		name: PROFILE,
 		component: ProfileView,
 		meta: { requiresAuth: true },
 	},
 
 	{
-		path: "/users",
+		path: '/users',
 		name: USERS,
 		component: UsersView,
 		meta: { requiresAuth: true },
 	},
 
 	{
-		path: "/managers",
+		path: '/managers',
 		name: MANAGERS,
 		component: ManagersView,
 		meta: { requiresAuth: true, requiredRoles: [ADMIN_ROLE] },
 		beforeEnter: roleGuard,
 	},
 
-  {
-    path: "/courses",
-    component: CoursesView,
-    meta: { requiresAuth: true },
-    children: [
-      {
-        path: "",
-        name: COURSE_DASHBOARD,
-        component: CoursesDashboardView,
-      },
-      {
-        path: ":id",
-        name: COURSE_DETAILS,
-        component: CourseDetailsView,
-        props: isValidId,
-      }
-    ],
-  },
 	{
-		path: "*",
+		path: '/courses',
+		component: CoursesView,
+		meta: { requiresAuth: true },
+		children: [
+			{
+				path: '',
+				name: COURSE_DASHBOARD,
+				component: CoursesDashboardView,
+			},
+			{
+				path: ':id',
+				name: COURSE_DETAILS,
+				component: CourseDetailsView,
+				beforeEnter: isCourseExist,
+			},
+		],
+	},
+	{
+		path: '*',
 		name: NOT_FOUND,
 		component: NotFoundView,
 		meta: { requiresAuth: true },
 	},
-]
+];
 
 const router = new VueRouter({
-	mode: "history",
+	mode: 'history',
 	base: process.env.BASE_URL,
 	routes,
-})
+});
 
-router.beforeEach(authGuard)
+router.beforeEach(authGuard);
 
-export default router
+export default router;
--- src/router/utils.js
+++ src/router/utils.js
@@ -1,31 +1,39 @@
-import { LOGIN } from "@/constants/routes.constant"
-import store from "../store/index"
+import { LOGIN, NOT_FOUND } from '@/constants/routes.constant';
+import { getAllCourses } from '@/api/course';
+import store from '../store/index';
+
 export const roleGuard = (to, from, next) => {
-	const user = store.getters["user/user"]
-		? store.getters["user/user"].role
-		: null
-	if (
-		to.matched.some((route) => route.meta.requiredRoles.includes(user))
-	) {
-		next()
+	const user = store.getters['user/user'] ? store.getters['user/user'].role : null;
+	if (to.matched.some((route) => route.meta.requiredRoles.includes(user))) {
+		next();
 	} else {
-		next({ path: "/" })
+		next({ path: '/' });
 	}
-}
+};
 
 export const authGuard = (to, from, next) => {
 	const user = JSON.parse(localStorage.getItem('user'));
 	if (to.matched.some((route) => route.meta.requiresAuth)) {
 		if (!user) {
-			store.dispatch('user/setUser', null, {root:true})
+			store.dispatch('user/setUser', null, { root: true });
 			next({
 				name: LOGIN,
-			})
+			});
 		} else {
-			store.dispatch('user/setUser', user, {root:true})
-			next()
+			store.dispatch('user/setUser', user, { root: true });
+			next();
 		}
 	} else {
-		next()
+		next();
 	}
-}
+};
+
+export const isCourseExist = async (to, _, next) => {
+	const courses = await getAllCourses();
+	const validIds = courses.map((course) => course.id);
+	if (validIds.includes(+to.params.id)) {
+		next();
+	} else {
+		next({ name: NOT_FOUND });
+	}
+};
