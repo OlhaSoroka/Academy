0bcf953033d40b460df47ccd2dcbbd67178a2194
Olha Soroka <osoroka0802@gmail.com>
2022-10-27 09:44:56 +0300

    added pinia toast store

--- frontend_vue3_ts/src/App.vue
+++ frontend_vue3_ts/src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div
-    class="flex relative"
-  > 
+  <div class="flex relative">
     <div class="bg-slate-400 ">Navigation menu</div>
     <div class="w-full h-screen overflow-hidden">
-      <div class="bg-sky-400 ">TopBar</div>
+      <div class="bg-sky-400 ">TopBar
+        <div class="m-3">
+          <button class="bg-red-500 text-white" @click="showFailureToast">fail</button>
+          <button class="bg-green-500 text-white" @click="showSuccessToast">success</button>
+        </div>
+      </div>
       <RouterView class="h-full overflow-auto bg-primary-100" />
     </div>
 
-    <!-- <ToastMessage /> -->
+    <ToastMessage />
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Options } from 'vue-class-component'
+import { mapStores } from 'pinia';
+import { ToastType, useToastStore } from './store/toast.store';
+import ToastMessage from './components/ToastMessage.vue';
 
-
-// Component definition
-@Options({
-  // Define component options
-
-})
-export default class Counter extends Vue {
-  // The behavior in class is the same as the current
-  
+export default {
+    computed: {
+        ...mapStores(useToastStore)
+    },
+    methods: {
+        showFailureToast() {
+            this.toastStore.showToastMessage({ message: "Oops! Something goes wrong...", type: ToastType.FAILURE });
+        },
+        showSuccessToast() {
+            this.toastStore.showToastMessage({ message: "User successfully updated.", type: ToastType.SUCCESS });
+        }
+    },
+    components: { ToastMessage }
 }
 </script>
