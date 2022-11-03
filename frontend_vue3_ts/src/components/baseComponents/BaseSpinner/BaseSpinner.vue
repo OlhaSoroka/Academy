4ec8111d1fd2d2247473a20f46c3d50ff600dc83
khemiiandrii <andrii.khemii@inventorsoft.co>
2022-11-03 10:59:11 +0200

    add base table, icons, select

---
+++ frontend_vue3_ts/src/components/baseComponents/BaseSpinner/BaseSpinner.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex justify-center border-none">
+    <div
+      style="border-top-color: transparent"
+      class="w-16 h-16 border-4 border-blue-400 border-double rounded-full animate-spin m-auto"
+    />
+  </div>
+</template>
